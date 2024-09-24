47ccbc81b2aedc18f65d7ce1de0161fcf1f759ef
acbc <acbc.dev@gmail.com>
2024-09-24 16:32:37 -0500

    (REMOVE) the content index js to use a chrome scripting

--- content/index.ts
+++
@@ -1,8 +0,0 @@
-import { onMessage } from "webext-bridge/content-script";
-
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-	onMessage<{ search: string; property: string }>("getdom", ({ data }) => {
-		const $$ = document.querySelectorAll(data.search);
-		console.log($$);
-	});
-});

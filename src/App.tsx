09fff19fceb4ddc2cc87d439d8e5db387da0a049
ACBC <acbc.dev@gmail.com>
2024-11-01 16:10:15 -0500

    componetizar

--- src/App.tsx
+++ src/App.tsx
@@ -1,150 +1,15 @@
 import "./App.css";
-import { Input } from "@/components/ui/input";
-import {
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableHeader,
-	TableRow,
-} from "@/components/ui/table";
-import { EllipsisVertical } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { useGlobal } from "@/hooks/useGlobal";
-import {
-	Dialog,
-	DialogContent,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
-} from "@/components/ui/dialog";
-import TableDialog from "@/components/TableDialog";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import Search from '@/components/Search';
+import RecentSearch from '@/components/RecentSearch';
+import TableResults from "@/components/TableResults";
 
 function App() {
-	const { search, result, property, handleSearch, history, updateSearch } =
-		useGlobal();
 	return (
 		<>
 			<main>
-				<search >
-					<form
-						className="flex items-center px-3 gap-x-2 "
-						onSubmit={(e) => {
-							e.preventDefault();
-							if (search !== null) {
-								handleSearch();
-							}
-						}}
-					>
-						<Input
-							autoFocus={true}
-							type="text"
-
-							placeholder="a, h1, p,.myclass"
-							value={search ?? ""}
-							onChange={(e) => updateSearch(e.target.value)}
-						/>
-						<Button type="submit" >
-							Search
-						</Button>
-					</form>
-				</search>
-				<section className="my-2 recent">
-					<h2 className="mt-2 text-xl">Recent Searches</h2>
-					<ScrollArea className="py-2 w-96 ">
-						<ul>
-							{history.map((i, index) => (
-								<li key={`${i}-${index}-${history.length}-${Math.random()}`}>
-									<Button
-										type="button"
-										onClick={() => {
-											updateSearch(i);
-											handleSearch(i);
-										}}
-									>
-										{i}
-									</Button>
-								</li>
-							))}
-						</ul>
-						<ScrollBar orientation="horizontal" />
-					</ScrollArea>
-				</section>
-				<ScrollArea className="w-full ">
-					<section className="result">
-						{result.length !== 0 && (
-							<>
-								<h2>Result</h2>
-								<Table className="scroolbar">
-									<TableHeader>
-										<TableRow>
-											<TableHead> </TableHead>
-											<TableHead>ID</TableHead>
-											<TableHead>Tag</TableHead>
-											{property.innerText && <TableHead>Inner Text</TableHead>}
-											{property.href && <TableHead>Href</TableHead>}
-											<TableHead>ClassList</TableHead>
-										</TableRow>
-									</TableHeader>
-									<TableBody className="text-start">
-										{result.map((item) => {
-											const { origin, pathname, search } = item.href
-												? new URL(item.href)
-												: { origin: "", pathname: "", search: "" };
-											return (
-												<>
-													<TableRow key={item.id}>
-														<TableCell>
-															<Dialog open={item.open}>
-																<DialogTrigger asChild>
-																	<EllipsisVertical className="duration-200 cursor-pointer hover:text-zinc-500" />
-																</DialogTrigger>
-																<DialogContent
-																	className="w-[95%] h-full"
-																	aria-describedby={`${item.id}-${item.tag}-${item.class}`}
-																>
-																	<DialogHeader>
-																		<DialogTitle>
-																			{item.tag}-{item.id}
-																		</DialogTitle>
-																	</DialogHeader>
-																	<section>
-																		<TableDialog element={item} />
-																	</section>
-																</DialogContent>
-															</Dialog>
-														</TableCell>
-														<TableCell>{item.id}</TableCell>
-														<TableCell>{item.tag}</TableCell>
-														{property.innerText && (
-															<TableCell>{item.innerText}</TableCell>
-														)}
-														{property.href && (
-															<TableCell>
-																<a
-																	href={item.href}
-																	className="underline hover:text-zinc-500"
-																	target="_blank"
-																	rel="noreferrer"
-																>
-																	{origin + pathname + (search && "?...")}
-																</a>
-															</TableCell>
-														)}
-														<TableCell className="w-fit">{item.class}</TableCell>
-													</TableRow>
-												</>
-											);
-										})}
-									</TableBody>
-								</Table>
-							</>
-						)}
-					</section>
-					<ScrollBar orientation="horizontal" />
-				</ScrollArea>
-
+				<Search />
+				<RecentSearch />
+				<TableResults />
 			</main>
 		</>
 	);
--- src/hooks/useGlobal.ts
+++ src/hooks/useGlobal.ts
@@ -8,13 +8,15 @@ export function useGlobal() {
 	const [property, setProperty] = useState<Record<string, boolean>>({});
 
 	useEffect(() => {
-		chrome.storage.local.get(["search", "history"], (data) => {
+		chrome.storage.local.get(["search", "history", 'result'], (data) => {
 			setSearch(data.search);
 			setHistory(data.history);
+			setResult(data.result);
 		});
 		chrome.runtime.onMessage.addListener((message) => {
 			if (message.type === "dom") {
 				if (Array.isArray(message.list)) {
+					chrome.storage.local.set({ result: message.list });
 					setResult(message.list);
 				}
 				setProperty(message.property);
@@ -22,13 +24,25 @@ export function useGlobal() {
 		});
 	}, []);
 
+	/**
+	 * Handles the search input and sends a message to the content script
+	 * to get the DOM elements.
+	 *
+	 * @param {string} [value] - The search value. If not provided, uses the
+	 * current search value.
+	 */
 	const handleSearch = (value = '') => {
 		const searchValue = value || search;
 		if (searchValue === null) return;
 		if (searchValue.trim() === "") return;
+
+		// Add the new search value to the beginning of the history
 		if (searchValue !== history[0]) setHistory([searchValue, ...history.slice(0, 10)]);
 
+		// Save the search value and history to local storage
 		chrome.storage.local.set({ search: searchValue, history });
+
+		// Get the currently active tab and execute the content script
 		chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 			chrome.scripting.executeScript({
 				target: { tabId: tabs[0].id ?? 0 },
@@ -36,6 +50,10 @@ export function useGlobal() {
 			});
 		});
 	};
+	const handleCopy = () => {
+		navigator.clipboard.writeText(JSON.stringify(result, null, 2));
+	}
+
 	const openModal = (id: number) => {
 		setResult((prev) =>
 			prev.map((item) => {
@@ -46,6 +64,29 @@ export function useGlobal() {
 			}),
 		);
 	};
+	/**
+	 * Handles exporting the search result as a JSON file.
+	 *
+	 * It creates a blob from the result data, creates a link with the blob URL,
+	 * adds the link to the page, triggers a click on the link to download the file,
+	 * and then removes the link and revokes the URL to avoid memory leaks.
+	 */
+	const handleExport = () => {
+		const data = JSON.stringify(result, null, 2);
+		const blob = new Blob([data], { type: "application/json" });
+		const url = URL.createObjectURL(blob);
+
+		const link = document.createElement("a");
+		link.href = url;
+		link.download = `${search?.replace(/\s/g, "-")}.json`;
+		// Activamos el enlace
+		document.body.appendChild(link);
+		link.click();
+
+		// Limpiamos el enlace y la URL creada
+		document.body.removeChild(link);
+		URL.revokeObjectURL(url);
+	}
 	const updateSearch = (newValue: string) => {
 		setSearch(newValue);
 	};
@@ -57,6 +98,8 @@ export function useGlobal() {
 		history: history,
 		updateSearch,
 		openModal,
+		handleExport,
+		handleCopy
 	};
 }
 function getDom() {
@@ -76,6 +119,7 @@ function getDom() {
 				width: item.offsetWidth,
 				height: item.offsetHeight,
 			},
+			content: item.content,
 			visible: (() => {
 				const rect = item.getBoundingClientRect();
 				return (
@@ -94,6 +138,7 @@ function getDom() {
 			},
 			boundingBox: item.getBoundingClientRect(),
 			childrenCount: item.children.length,
+
 		}));
 
 		const property: Record<string, boolean> = {

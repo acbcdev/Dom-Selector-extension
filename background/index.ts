48d44fa789b8d44e7aebc2145f1a5047bf239f1c
acbc <acbc.dev@gmail.com>
2025-04-07 18:45:19 -0500

    style: improve code formatting and consistency across multiple files

--- background/index.ts
+++ background/index.ts
@@ -1,3 +1,7 @@
 chrome.runtime.onInstalled.addListener(() => {
-	chrome.storage.local.set({ search: "", history: ['img', 'video', 'source'], result: [] });
+	chrome.storage.local.set({
+		search: "",
+		history: ["img", "video", "source"],
+		result: [],
+	});
 });
